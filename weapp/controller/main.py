25855ebe50f576cf52273cb0140a0346a63fcbd4
yueyt <yyt030@gmail.com>
2017-07-25 14:51:31 +0800

    update

--- weapp/controller/main.py
+++ weapp/controller/main.py
@@ -39,5 +39,5 @@ async def signature(request):
         print('>>>', request.body)
         msg = parse_message(request.body)
         print('>>>>>', msg)
-
-    return
+        return text(msg)
+    return text('')
