e415fd9caf9a0e286f9c8b064c015d3e9f867060
yueyt <yyt030@gmail.com>
2017-07-26 23:40:49 +0800

    update

--- config.py
+++ config.py
@@ -14,16 +14,16 @@ class Config(object):
     WECHAT_APPID = os.environ.get('WECHAT_APPID') or 'hard to guess string'
 
     WELCOME_MSG = '''
-     欢迎使用wechat-robot,
-     你可以试试如下命令：命令 内容
-     
+     欢迎使用wechat-robot,你可以试试如下命令：
      快递 XXX
      天气 XXX
      翻译 XXX
-     
-     如：要翻译：我是谁？，请输入： 翻译 我是谁？
-        要查询快递：12345678，请输入：快递 12345678
-     
+     ...
+     如：
+     要翻译：我是谁？，请输入： 
+     翻译 我是谁？
+     要查询快递：12345678，请输入：
+     快递 12345678
     '''
 
     @classmethod
