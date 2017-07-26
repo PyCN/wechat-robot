8c018e29ff7463f48fa2b9d07bde1cc5c73627d9
yueyt <yyt030@gmail.com>
2017-07-26 19:19:26 +0800

    update

--- weapp/controller/main.py
+++ weapp/controller/main.py
@@ -3,9 +3,8 @@
 
 __author__ = 'yueyt'
 
-from sanic import Blueprint
+from sanic import Blueprint, response
 from sanic.log import log, netlog
-from sanic.response import text
 from translate import Translator
 from wechatpy import parse_message
 from wechatpy.crypto import WeChatCrypto
@@ -18,7 +17,7 @@ bp = Blueprint('main', __name__)
 
 @bp.route('/', methods=['GET', 'POST'])
 async def index(request):
-    return text('hello, world')
+    return response.redirect('http://www.sina.com.cn')
 
 
 @bp.route('/interface', methods=['GET', 'POST'])
@@ -29,29 +28,30 @@ async def interface(request):
     appid = request.app.config.get('WECHAT_APPID', None)
 
     # get var from request args
-    signature = request.raw_args.get('signature')
-    timestamp = request.raw_args.get('timestamp')
-    nonce = request.raw_args.get('nonce')
-    echostr = request.raw_args.get('echostr')
-    msg_signature = request.raw_args.get('msg_signature')
-    encrypt_type = request.raw_args.get('encrypt_type')
+    signature = request.raw_args.get('signature', '')
+    timestamp = request.raw_args.get('timestamp', '')
+    nonce = request.raw_args.get('nonce', '')
+    echostr = request.raw_args.get('echostr', '')
+    msg_signature = request.raw_args.get('msg_signature', '')
+    encrypt_type = request.raw_args.get('encrypt_type', '')
 
     try:
         check_signature(token, signature, timestamp, nonce)
     except InvalidSignatureException:
         # 处理异常情况或忽略
         netlog.error('>>> {},{}'.format(request.raw_args, '验证异常'))
-        return text('验证异常')
+        return response.text('验证异常')
     else:
         if request.method == 'GET':
+            # 服务器配置
             log.info('>>> {},{}'.format(request.raw_args, '验证ok'))
-            return text(echostr)
+            return response.text(echostr)
         else:
-            # 验证成功后, 接受消息
+            # 公众号被动接受消息
             if len(request.body) == 0:
-                return text('')
+                return response.text('')
 
-            # 解密报文
+            # 加密方式
             if encrypt_type == 'aes':
                 crypto = WeChatCrypto(token, encoding_aes_key, appid)
                 try:
@@ -63,10 +63,11 @@ async def interface(request):
                     )
                 except (InvalidAppIdException, InvalidSignatureException):
                     # to-do: 处理异常或忽略
-                    return text('')
+                    return response.text('')
                 else:
                     request_msg = parse_message(decrypted_xml)
             else:
+                # 纯文本方式
                 request_msg = parse_message(request.body)
 
             request_msg_type = request_msg.type
@@ -74,7 +75,6 @@ async def interface(request):
                                                                                         request_msg))
             # 根据消息类型解析
             if request_msg_type == 'text':
-                # reply = TextReply(content='你所发的是文本:{}'.format(request_msg.content), message=request_msg)
                 reply = text_reply(request_msg.content, request_msg)
             elif request_msg_type == 'image':
                 reply = ImageReply(message=request_msg)
@@ -87,26 +87,26 @@ async def interface(request):
 
             # 返回xml报文
             xml = reply.render()
-            return text(xml)
+            return response.text(xml)
 
 
 def text_reply(text, req_msg):
     do_type = text[:2]
-    if do_type == '翻译':
-        resp = translate_text(text[2:])
-    elif do_type == '快递':
-        resp = kudi_text(text[2:])
+    if do_type == '翻译' or do_type == 'fy':
+        resp = text_translate(text[2:])
+    elif do_type == '快递' or do_type == 'kd':
+        resp = text_kuaidi(text[2:])
     else:
         resp = 'ooo, 不明白你的意思!'
     return TextReply(content='{}'.format(resp), message=req_msg)
 
 
-def translate_text(text):
+def text_translate(text):
     if not text:
         return None
     translator = Translator(from_lang='zh', to_lang="en")
     return translator.translate(text)
 
 
-def kudi_text(text):
+def text_kuaidi(text):
     pass
