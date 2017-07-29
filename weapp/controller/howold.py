e9a2f891b271105879539878fe1dc89e36c6b0e1
yueyt <yyt030@gmail.com>
2017-07-30 00:42:42 +0800

    fix pic

--- weapp/controller/howold.py
+++ weapp/controller/howold.py
@@ -34,23 +34,29 @@ def howold(picurl):
     resp = json.loads(r.text)
     resp = json.loads(resp)
     AnalyticsEvent = resp.get('AnalyticsEvent')
-    face = json.loads(AnalyticsEvent)[0].get('face')
-    if not face:
-        return random.choice(['算你狠，居然没有识别出来', '算了吧，不确定，是不是无性别人士'])
-    else:
+
+    result = []
+    for i in json.loads(AnalyticsEvent):
+        face = i.get('face')
         age = face.get('age', '不知道多大')
+
+        # update
         if face.get('gender') == 'Female':
-            if age < 5:
+            if age < 10:
                 gender = '且漂亮可爱的小姑娘'
             else:
                 gender = '且漂亮可爱的靓妹'
         else:
             gender = '且英气逼人的帅哥'
+        result.append('一位年龄大约{}岁，{}'.format(age, gender))
 
-        return '我看到一位年龄大约{}岁，{}'.format(age, gender)
+    if len(result) == 0:
+        return random.choice(['算你狠，居然没有识别出来', '算了吧，不确定，是不是无性别人士'])
+    else:
+        return '我看到{}位，{}'.format(len(result), ','.join(result))
 
 
 if __name__ == '__main__':
-    picurl = ''
-    res = howold(picurl)
-    print(res)
+    pic = ''
+
+    print(howold(pic))
